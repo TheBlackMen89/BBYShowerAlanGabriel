e396fdcd53ab84930eb638cf93ae7ce1a0c936ec
TheBlackMen89 <ojavierparra@hotmail.com>
2026-07-21 02:11:23 -0500

    Clase 6 - Loader inicial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
-console.log("Bienvenido al Baby Shower de Alan Gabriel");
+// ============================
+// ELEMENTOS DEL DOM
+// ============================
+
+const btnMotor = document.querySelector("#btnMotor");
+const loader = document.querySelector("#loader");
+
+// ============================
+// FUNCIONES
+// ============================
+
+function ocultarLoader() {
+
+    setTimeout(() => { 
+    loader.classList.add("oculto");
+
+    }, 4000);
+
+}
+
+function encenderMotor() {
+
+    alert("🏍️ ¡Motor encendido!");
+
+}
+
+// ============================
+// EVENTOS
+// ============================
+
+window.addEventListener("load", () => {
+
+    ocultarLoader();
+
+});
+
+btnMotor.addEventListener("click", () => {
+    
+    encenderMotor();
+
+});
